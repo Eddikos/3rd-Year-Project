8e034369c27fc1cdb0da8d527d8b055b41a11b0e
Eddikos <eldar.alasgarov@hotmail.com>
2015-01-29 15:29:14 +0000

    Initial Files - Generated using Youtube tutorial

---
+++ js/app/content.js
@@ -0,0 +1,47 @@
+﻿//alert('content script loaded');
+
+// Every page (in browser) is going to have this CONTENT script
+
+// First thng that it does is Adds a LISTENET and starts listen to upcoming messages
+chrome.extension.onMessage.addListener(
+function (request, sender, sendResponse) {
+
+    debugger;
+
+    // And it will listen specifically for this PageInfo action
+    if (request.action == 'PageInfo') {
+        var pageInfos = [];
+        
+        // Normal jQuery stuff
+        $('a').each(function() {
+            var pageInfo = {};
+            
+            var href = $(this).attr('href');
+
+            if (href != null && href.indexOf("http") == 0)
+            {
+                //only add urls that start with http
+                pageInfo.url = href
+                // Create and Array of URLs basically :)
+                pageInfos.push(pageInfo);
+            }
+        });
+        
+        $('img').each(function() {
+            var pageInfo = {};
+            
+            var alt = $(this).attr('alt');
+
+            if (alt != null)
+            {
+                //only add urls that start with http
+                pageInfo.url = alt
+                // Create and Array of URLs basically :)
+                pageInfos.push(pageInfo);
+            }
+        });
+        
+        // Send response Back to POPUP.js
+        sendResponse(pageInfos);
+    }
+});
---
+++ js/app/popup.js
@@ -0,0 +1,44 @@
+﻿myApp.service('pageInfoService', function() {
+    this.getInfo = function(callback) {
+        var model = {};
+        
+        // Find active tabs, i.e. tab you are browsing
+        chrome.tabs.query({'active': true},
+        function (tabs) {
+            if (tabs.length > 0)
+            {
+                // Grab the title and URL of the page you are currently on
+                model.title = tabs[0].title;
+                model.url = tabs[0].url;
+                
+                // Then Send message directly to that one tab
+                // i.e. you are sending to the CONTENT.js script
+                // which is listening for messages
+                // And when the response comes from CONTENT.js, it will apply it straight away
+                chrome.tabs.sendMessage(tabs[0].id, { 'action': 'PageInfo' }, function (response) {
+                    model.pageInfos = response;
+                    callback(model);
+                });
+            }
+
+        });
+    };
+});
+
+myApp.controller("PageController", function ($scope, pageInfoService) {
+    // Will be passed to the PageController which is in the POPUP.html
+    $scope.message = "This is a demo extension for Web2Access Accessibility tool";  
+    
+    // Call the function which is up, and use the varaible Callback, straight away... INFO
+    pageInfoService.getInfo(function (info) {
+        // These variables are also passed to POPUP.html
+        $scope.title = info.title;
+        $scope.url = info.url;
+        $scope.pageInfos = info.pageInfos;
+        
+        $scope.$apply();
+    });
+});
+
+
+
