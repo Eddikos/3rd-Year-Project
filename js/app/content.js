505280b677f8aad0c5ce4fffea3dd7f458f1b954
Eddikos <eldar.alasgarov@hotmail.com>
2015-02-01 18:28:12 +0000

    Tests - triggered only when called,
    Not all at the same time.
    
    Task:
    Clean up Popup.js, contents.js and testn.htmls

--- js/app/content.js
+++ js/app/content.js
@@ -7,50 +7,71 @@ chrome.extension.onMessage.addListener(
 function (request, sender, sendResponse) {
 
     //debugger;
+        
+        
 
     // And it will listen specifically for this PageInfo action
     if (request.action == 'PageInfo') {
-        var pageInfos = [];
         var urls = [];
-        var imgs = [];
         var numberOfLinks = 0;
-        var numberOfImages = 0;
-        var numberOfImagesWithAlt = 0;
-        var numberOfImagesWithEmptyAlt = 0
-
         // Normal jQuery stuff
         $('a').each(function() {
             numberOfLinks++;
 
-            var pageInfo = {};            
+            var pageLink = {};            
 
             var href = $(this).attr('href');
 
             if (href != null && href.indexOf("http") == 0)
             {
                 //only add urls that start with http
-                pageInfo.url = href;
+                pageLink.url = href;
                 // Create and Array of URLs basically :)
 
-                console.log(pageInfo);
-                urls.push(pageInfo);
+                console.log(pageLink);
+                urls.push(pageLink);
             }
         });
         
-        pageInfos.push(urls);
         console.log(urls);
-        console.log(pageInfos);
 
 
 
+
+        // // Storing the data in Chrome.Storage.Sync
+        // console.log(imgCount);
+
+        // chrome.storage.sync.set({'value': imgCount}, function() {
+        //     chrome.storage.sync.get("value", function(data) {
+        //         console.log("data", data.value);
+        //     });
+        // });
+
+        // Send response Back to POPUP.js
+        sendResponse(urls);
+    }
+
+    if (request.action == 'PageImages') {
+        var imgs = [];
+        
+        var numberOfImages = 0;
+        var numberOfImagesWithAlt = 0;
+        var numberOfImagesWithEmptyAlt = 0
+        
 
         var imgCount = 0;
-        $('img').each(function() {
+        $('img').each(function(index) {
             numberOfImages++;
 
             var pageInfo = {};
             
             var alt = $(this).attr('alt');
+          
+            $(this).addClass("blue");
+            $(this).attr("ng-class","myclass");
+            $(this).addClass("" + index + "");
+
+            console.log($(this));
 
             if (alt != null)
             {
@@ -65,18 +86,9 @@ function (request, sender, sendResponse) {
             }
         });
 
-        pageInfos.push(imgs);
+        
+        console.log(imgs);
 
-        // // Storing the data in Chrome.Storage.Sync
-        // console.log(imgCount);
-
-        // chrome.storage.sync.set({'value': imgCount}, function() {
-        //     chrome.storage.sync.get("value", function(data) {
-        //         console.log("data", data.value);
-        //     });
-        // });
-
-        // Send response Back to POPUP.js
-        sendResponse(pageInfos);
+        sendResponse(imgs);
     }
 });
--- js/app/popup.js
+++ js/app/popup.js
@@ -1,51 +1,57 @@
-﻿myApp.service('pageInfoService', function() {
-    this.getInfo = function(callback) {
-        var model = {};
-        
-        // Find active tabs, i.e. tab you are browsing
-        chrome.tabs.query({'active': true},
-        function (tabs) {
-            if (tabs.length > 0)
-            {
-                // Grab the title and URL of the page you are currently on
-                model.title = tabs[0].title;
-                model.url = tabs[0].url;
-                
-                // Then Send message directly to that one tab
-                // i.e. you are sending to the CONTENT.js script
-                // which is listening for messages
-                // And when the response comes from CONTENT.js, it will apply it straight away
-                chrome.tabs.sendMessage(tabs[0].id, { 'action': 'PageInfo' }, function (response) {
-                    model.pageInfos = response;
-                    callback(model);
-                });
-            }
-
-        });
-    };
-});
-
-myApp.controller("PageController", function ($scope, pageInfoService) {
+﻿myApp.controller("PageController", function ($scope) {
     // Will be passed to the PageController which is in the POPUP.html
-    $scope.message = "This is a demo extension for Web2Access Accessibility tool";  
-    
-    // Call the function which is up, and use the varaible Callback, straight away... INFO
-    pageInfoService.getInfo(function (info) {
-        // These variables are also passed to POPUP.html
-        $scope.title = info.title;
-        $scope.url = info.url;
-        $scope.pageInfos = info.pageInfos;
+    $scope.message = "This is a demo extension for Web2Access Accessibility tool"; 
         
-        $scope.$apply();
+    $scope.tests = [ { name: 'Login, Signup and Other Forms Accessible', url: 'tests/test1.html', number: 0},
+                    { name: 'Image ALT Attributes', url: 'tests/test2.html', number: 1},
+                    { name: 'Link Target Definitions', url: 'tests/test3.html', number: 2},
+                    { name: 'Removal of Stylesheet', url: 'tests/test4.html', number: 3},
+                    { name: 'Appropriate use of Tables', url: 'tests/test5.html', number: 4} ];
+
+    // Default Value for test, to start from 1
+    $scope.test = $scope.tests[1];
+
+    // Buttons for Previous/Next test
+    $scope.back = function() {
+        $scope.test = $scope.tests[$scope.test.number-1];
+    }
+
+    $scope.forward = function() {
+        $scope.test = $scope.tests[$scope.test.number+1];
+
+        if ($scope.test.number == 2){
+             $scope.requestLinks();
+        }
+    }
+
+
+
+    // Find active tabs, i.e. tab you are browsing
+    chrome.tabs.query({'active': true},
+    function (tabs) {
+        if (tabs.length > 0)
+        {
+            // Grab the title and URL of the page you are currently on
+            $scope.title = tabs[0].title;
+            $scope.url = tabs[0].url;
+            
+            // Then Send message directly to that one tab
+            // i.e. you are sending to the CONTENT.js script
+            // which is listening for messages
+            // And when the response comes from CONTENT.js, it will apply it straight away
+            chrome.tabs.sendMessage(tabs[0].id, { 'action': 'PageImages' }, function (response) {
+                $scope.pageImages = response;
+                $scope.$apply();
+            });
+
+            $scope.requestLinks = function(){
+                chrome.tabs.sendMessage(tabs[0].id, { 'action': 'PageInfo' }, function (response) {
+                    $scope.pageLinks = response;
+                    $scope.$apply();
+                });
+            };
+        }
     });
-
-
-    $scope.tests =
-    [ { name: 'test1.html', url: 'tests/test1.html'},
-      { name: 'test2.html', url: 'tests/test2.html'},
-      { name: 'test3.html', url: 'tests/test3.html'},
-      { name: 'test4.html', url: 'tests/test4.html'},
-      { name: 'test5.html', url: 'tests/test5.html'} ];
-    $scope.test = $scope.tests[0];
-
 });
+
+            
